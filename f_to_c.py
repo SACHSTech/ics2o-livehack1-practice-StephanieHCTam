599afc60a77b9ea7e992496e18342374badf2af5
Stephanie Tam <hoiching.tam23@ycdsbk12.ca>
2021-02-08 15:04:57 +0000

    Completed f_to_c.py

--- f_to_c.py
+++ f_to_c.py
@@ -1,1 +1,19 @@
+"""
+-------------------------------------------------------------------------------
+Name:     f_to_c.py
+Purpose:  This program lets user enter a Fahrenheit degree and convert to celsius degree.
 
+Author:   Tam.S
+
+Created:  08/02/2021
+------------------------------------------------------------------------------
+"""
+
+# get Fahrenheit degree
+fahrenheit = float(input("Enter degree measure in Fahrenheit: "))
+
+# compute Fahrenheit to Celsius
+celsius = (fahrenheit - 32) * 5.0 / 9.0
+
+# output result
+print("Celsius degree measure is: " + str(celsius))
