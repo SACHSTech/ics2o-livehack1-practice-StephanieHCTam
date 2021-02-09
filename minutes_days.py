b1d44f75129f52f26299d18dcec0c70fb5debd97
Stephanie Tam <hoiching.tam23@ycdsbk12.ca>
2021-02-09 13:38:20 +0000

    Completed minutes_days.py

--- minutes_days.py
+++ minutes_days.py
@@ -1,1 +1,21 @@
+"""
+-------------------------------------------------------------------------------
+Name:     minutes_days.py
+Purpose:  This program lets user enter a number of minutes and that will calculate the number of days, hours and minutes that represents.
 
+Author:   Tam.S
+
+Created:  08/02/2021
+------------------------------------------------------------------------------
+"""
+
+# get number of minutes
+minutes = int(input("Enter number of minutes: "))
+
+# compute minutes to days, hours, and minutes
+days = int(minutes // 1440)
+hours = int(minutes // 60 - (days * 24))
+minutes_remaining = int(minutes - (days * 1440) - (hours * 60))
+
+# output results
+print(str(days) + " days " + str(hours) + " hours and " + str(minutes_remaining) + " minutes.")
--- windchill.py
+++ windchill.py
@@ -1,1 +1,20 @@
+"""
+-------------------------------------------------------------------------------
+Name:     windchill.py
+Purpose:  This program lets gets the temperature (celsius) and wind speed (km/h) then computes and outputs the windchill factor.
 
+Author:   Tam.S
+
+Created:  08/02/2021
+------------------------------------------------------------------------------
+"""
+
+# get information
+temperature = float(input("Enter the temperature in celsius: "))
+wind_speed = float(input("Enter the wind speed in km/h: "))
+
+# compute windchill factor
+wind_chill = 13.12 + (0.6215 * temperature) - (11.37 * wind_speed ** 0.16) + (0.3965 * temperature * wind_speed ** 0.16)
+
+# output result
+print("The wind chill is", str(wind_chill), "degrees celsius.")
